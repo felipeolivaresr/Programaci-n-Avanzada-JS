b570845076a6f7c4da3383a84935bf96cba8cf85
felipeolivaresr <olivaresroblesfelipe@gmail.com>
2024-11-30 15:23:49 -0300

    Actualización de error de sonido

--- assets/js/animalesService.js
+++ assets/js/animalesService.js
@@ -1,8 +1,48 @@
 export const obtenerAnimales = async () => {
     try {
-        const response = await fetch('../../animales.json');
-        const data = await response.json();
-        return data.animales;
+        
+        const animalesData = {
+            "animales": [
+                {
+                    "name": "Leon",
+                    "imagen": "Leon.png",
+                    "sonido": "Rugido.mp3"
+                },
+                {
+                    "name": "Lobo",
+                    "imagen": "Lobo.jpg",
+                    "sonido": "Aullido.mp3"
+                },
+                {
+                    "name": "Oso",
+                    "imagen": "Oso.jpg",
+                    "sonido": "Gruñido.mp3"
+                },
+                {
+                    "name": "Serpiente",
+                    "imagen": "Serpiente.jpg",
+                    "sonido": "Siseo.mp3"
+                },
+                {
+                    "name": "Aguila",
+                    "imagen": "Aguila.png",
+                    "sonido": "Chillido.mp3"
+                }
+            ]
+        };
+
+        try {
+            const response = await fetch('./animales.json');
+            if (response.ok) {
+                const data = await response.json();
+                return data.animales;
+            }
+        } catch (error) {
+            console.warn('Usando datos de respaldo:', error);
+        }
+
+       
+        return animalesData.animales;
     } catch (error) {
         console.error('Error al obtener animales:', error);
         throw error;
